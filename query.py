7698548b8638f1bb763b07193d89048790ad0647
Sebastian Nunez <senu9248@gmail.com>
2024-05-16 00:57:16 -0700

    frame extract from videos

--- query.py
+++ query.py
@@ -31,21 +31,22 @@ def process_image(image_path, model, tokenizer, prompt):
     with torch.cuda.amp.autocast():
         response, _ = model.chat(tokenizer, query=prompt, image=image_path, history=[], do_sample=False) #do_sample=False for shorter responses
     return response
-def process_directory(directory, model, tokenizer, prompt, output_file, n):
+
+def process_directory(directory, model, tokenizer, prompt_list, output_file, n):
     with open(output_file, 'w') as file:
         for filename in os.listdir(directory):
             image_path = os.path.join(directory, filename)
-            result_list = process_imagenx(image_path, model, tokenizer, prompt, n)
+            result_list = process_imagenx(image_path, model, tokenizer, prompt_list, n)
             file.write(f"Filename: {filename}\n")
             for i, result in enumerate(result_list):
                 file.write(f"{result}\n")
             file.write('\n')
             print(f"Result for {filename}: {result_list}")
 
-def process_imagenx(image_path, model, tokenizer, prompt, n):
+def process_imagenx(image_path, model, tokenizer, prompt_list, n):
     response_list= []
     with torch.cuda.amp.autocast():
-        for current_prompt in prompt:
+        for current_prompt in prompt_list:
             current_prompt_list = []
             for i in range(n):
                 response = query(image_path, model, tokenizer, current_prompt)
@@ -59,10 +60,12 @@ def query(image_path, model, tokenizer, prompt):
     return response
 
 
-directory_path = 'gtframes'
+directory_path = 'sampleframes'
 scene_prompt = '<ImageHere> Please using only one word describe if the scene is outdoor or indoor.'
 lighting_prompt = '<ImageHere> Please using only one word describe if the lighting in the image is bad or good.'
 dynamic_prompt = '<ImageHere> Imagine we are trying to split images into categories where the objects and people are mostly static or dynamic. Please reply with only one word static or dynamic for this image'
 script_directory = os.path.dirname(os.path.abspath(__file__))  # Get the directory where the script is located
-output_file = os.path.join(script_directory, 'results.txt')  # Output file in the same directory as the script
+output_file = os.path.join(script_directory, 'sampleresults.txt')  # Output file in the same directory as the script
 process_directory(directory_path, model, tokenizer, [scene_prompt, lighting_prompt, dynamic_prompt], output_file, 3)
+
+
