b6448b5648f5d5e069045d9b824fa7c077eb8271
Sebastian Nunez <senu9248@gmail.com>
2024-05-17 02:28:34 -0700

    Metrics Class and upload gt_images

--- compare.py
+++ compare.py
@@ -7,9 +7,7 @@ def process_line(line, sample_voted, majority_vote_list):
         current_filename = line.split(': ')[1].strip()
         sample_voted['File'].append(current_filename)
     elif line.startswith('['):
-        # Clean line and just keep result1, result2, result3. Then split into list
         clean_line = line.strip().replace('[', '').replace(']', '').replace("'", '').split(", ")
-        # Take majority vote
         majority_vote_list.append(max(set(clean_line), key=clean_line.count))
 
 def categorize_samples(arrange_list, sample_voted):
@@ -45,4 +43,54 @@ def create_dataframe(file_path):
 
 predictions = create_dataframe('sampleresults.txt')
 ground_truth = pd.read_csv('sampletruth.csv')
-print(predictions.to_string())
+
+
+class Metrics:
+    def __init__(self, ground_truth, predictions):
+        self.gt = ground_truth
+        self.pred = predictions
+
+
+    def summary(self):
+        summary_data = {}
+        total_conflicts = self.gt.compare(self.pred).shape[0]
+        total_entries = self.gt.shape[0]
+        percent_conflicts = round(total_conflicts / total_entries * 100, 2)
+        summary_data['Conflicted Images'] = [total_conflicts, percent_conflicts]
+
+        for column in self.gt.drop(columns=['File']).columns:
+            conflict_count = self.gt[self.gt[column] != self.pred[column]].shape[0]
+            percent_conflicts = round(conflict_count / total_entries * 100, 2)
+            summary_data[column] = [conflict_count, percent_conflicts]
+
+        summary_df = pd.DataFrame.from_dict(summary_data).T
+        summary_df = summary_df.rename(columns={0: "Count", 1: "Percent"})
+        print(summary_df.to_string())
+
+
+    def display_conflict(self):
+        compared_data = self.gt.compare(self.pred, keep_shape=True, keep_equal=False)
+        compared_data['File'] = self.gt['File']
+
+        conflict_dict = {'File': self.gt['File']}
+
+        for column in self.gt.columns:
+            if column != 'File':
+                conflict_dict[f'gt_{column}'] = self.gt[column]
+                conflict_dict[f'pred_{column}'] = self.pred[column]
+
+        conflict_df = pd.DataFrame(conflict_dict)
+
+        difference_mask = self.gt.drop(columns=['File']) != self.pred.drop(columns=['File'])
+        rows_with_multiple_conflicts = difference_mask.sum(axis=1) >= 1
+        conflict_df = conflict_df[rows_with_multiple_conflicts]
+
+        print(conflict_df.to_string())
+
+
+
+
+Results = Metrics(ground_truth, predictions)
+Results.summary()
+#print(ground_truth.compare(predictions))
+#print(pd.merge(ground_truth, predictions, how='left', on=['File']))
