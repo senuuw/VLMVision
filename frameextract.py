3eda7d82e3c77e75e1e5044388d0a4f44eeb4c66
Sebastian Nunez <senu9248@gmail.com>
2024-06-25 19:17:41 -0700

    restructure query for dataframes

--- frameextract.py
+++ frameextract.py
@@ -1,8 +1,6 @@
 import cv2
 import os
 
-videos = os.listdir(r"D:\ego4d\v2\full_scale")[10:20]
-print(videos)
 
 
 
@@ -29,11 +27,12 @@ def extract_frames(path, video_num):
             print(f'Video {video_num}, path:{path} failed cap.read()')
 
 
-def extract_indexvideo_frames(start_n, end_n, path):
-    video_count = start_n
-    videos = os.listdir(path)[start_n:end_n]
+def extract_directory_frames(path, n):
+    video_count = 0
+    videos = os.listdir(path)[1:n+1] #start at 1 to ignore manifest)
     for video in videos:
-        count = 0
+        print(f'Starting Video {video}')
+        timer_count = 0
         current_vid_path = os.path.join(r"D:\ego4d\v2\full_scale", video)
         cap = cv2.VideoCapture(current_vid_path)
         frame_count = 0
@@ -47,13 +46,13 @@ def extract_indexvideo_frames(start_n, end_n, path):
                         break
                     else:
                         frame_count += 1
-                        cv2.imwrite(f'sampleframes/video{video_count}_frame{frame_count}.jpg', frame)
-                        count += 600 # i.e. at 30 fps, this advances one second, currently every 20s
-                        cap.set(cv2.CAP_PROP_POS_FRAMES, count)
+                        cv2.imwrite(f'frames/{video[:-4]}_{frame_count:04d}.jpg', frame)
+                        timer_count += 600 # i.e. at 30 fps, this advances one second, currently every 20s
+                        cap.set(cv2.CAP_PROP_POS_FRAMES, timer_count)
                 else:
                     cap.release()
                     break
             except:
                 print(f'Video {video_count}, path:{current_vid_path} failed cap.read()')
 
-extract_frames(r"D:\ego4d\v2\full_scale\29a90a49-4137-4f81-b24d-2d923ab8fb55.mp4", 21)
+extract_directory_frames(r"D:\ego4d\v2\full_scale", 30)
--- query.py
+++ query.py
@@ -1,9 +1,9 @@
 import os
 import torch, auto_gptq
-import numpy as np
+import pandas as pd
 from transformers import AutoModel, AutoTokenizer
 from auto_gptq.modeling import BaseGPTQForCausalLM
-
+from brightblur import detect_blur_and_bright_spot
 auto_gptq.modeling._base.SUPPORTED_MODELS = ["internlm"]
 torch.set_grad_enabled(False)
 
@@ -20,34 +20,59 @@ class InternLMXComposer2QForCausalLM(BaseGPTQForCausalLM):
         ["feed_forward.w2.linear"],
     ]
 
-
 # init model and tokenizer
 model = InternLMXComposer2QForCausalLM.from_quantized(
     'internlm/internlm-xcomposer2-vl-7b-4bit', trust_remote_code=True, device="cuda:0").eval()
 tokenizer = AutoTokenizer.from_pretrained(
     'internlm/internlm-xcomposer2-vl-7b-4bit', trust_remote_code=True)
 
-def process_directory(directory, model, tokenizer, prompt_list, output_file, n):
-    with open(output_file, 'w') as file:
-        for filename in os.listdir(directory):
-            image_path = os.path.join(directory, filename)
-            result_list = process_imagenx(image_path, model, tokenizer, prompt_list, n)
-            file.write(f"{filename},")
-            for i, result in enumerate(result_list):
-                file.write(f"{result}\n")
-            file.write('\n')
-            print(f"Result for {filename}: {result_list}")
+def process_directory(directory, model, tokenizer, prompt_list, n):
+    # Dictionary that is turned into dataframe
+    classification_dict = {
+        'Filename': [],
+        'Setting': [],
+        'Lighting': [],
+        'Motion': [],
+        'Blur': [],
+        'Bright Spot': []
+    }
+
+    for file_name in os.listdir(directory):
+        image_path = os.path.join(directory, file_name)
+
+        # Query VLM for setting, lighting, motion and use OpenCV detection for blur and bright spot
+        setting_list, lighting_list, motion_list = process_imagenx(image_path, model, tokenizer, prompt_list, n)
+        blur, bright_spot = detect_blur_and_bright_spot(image_path)
+        keys = list(classification_dict.keys())
+
+        # Add to dictionary
+        for i, classification in enumerate([file_name, setting_list, lighting_list, motion_list, blur, bright_spot]):
+            classification_dict[keys[i]].append(classification)
+        print(f"{file_name} done")
+
+    # Convert to DataFrame with pandas
+    data_frame = pd.DataFrame.from_dict(classification_dict)
+    print(data_frame)
+    return data_frame
+
 
 def process_imagenx(image_path, model, tokenizer, prompt_list, n):
-    response_list= []
+    response_list = []
+
+    # autocast() to ensure that all torch objects on GPU
     with torch.cuda.amp.autocast():
+
+        # Choose prompt
         for current_prompt in prompt_list:
             current_prompt_list = []
+
+            # n is the amount of times each prompt is asked
             for i in range(n):
                 response = query(image_path, model, tokenizer, current_prompt)
                 current_prompt_list.append(response)
+
             response_list.append(current_prompt_list)
-    return response_list
+    return response_list[0], response_list[1], response_list[2]
 
 
 def query(image_path, model, tokenizer, prompt):
@@ -55,12 +80,12 @@ def query(image_path, model, tokenizer, prompt):
     return response
 
 
-directory_path = 'sampleframes'
+directory_path = 'frames'
 scene_prompt = '<ImageHere> Please using only one word describe if the scene is outdoor or indoor.'
 lighting_prompt = '<ImageHere> Please using only one word describe if the lighting in the image is bad or good.'
-dynamic_prompt = '<ImageHere> Imagine we are trying to split images into categories where the objects and people are mostly static or dynamic. Please reply with only one word static or dynamic for this image'
+dynamic_prompt = '<ImageHere> Imagine we are trying to split images into categories where the objects and people are mostly static or dynamic, please answer with only one word static or dynamic for this image.'
 script_directory = os.path.dirname(os.path.abspath(__file__))  # Get the directory where the script is located
-output_file = os.path.join(script_directory, 'sampleresults.txt')  # Output file in the same directory as the script
-process_directory(directory_path, model, tokenizer, [scene_prompt, lighting_prompt, dynamic_prompt], output_file, 3)
+data_frame = process_directory(directory_path, model, tokenizer, [scene_prompt, lighting_prompt, dynamic_prompt], 3)
+data_frame.to_pickle('results.pkl')
 
 
